ce4cb49415fff37370d6dee5dbc1e8da6203ddca
Moises Mariano <moisesduartem@gmail.com>
2020-12-10 14:11:51 -0300

    implements select & sql getters and setters

--- SqlBuilder.cpp
+++ SqlBuilder.cpp
@@ -5,6 +5,33 @@ SqlBuilder::SqlBuilder(QString table)
   this->table = table;
 }
 
+/**
+ * @brief SqlBuilder::select
+ * @param columns
+ * @return
+ */
+SqlBuilder* SqlBuilder::select(QStringList columns) {
+    this->columns = columns;
+    this->sql += "select " + columns.join(", ") + " from " + this->table;
+    return this;
+}
+
+/**
+ * @brief SqlBuilder::setSql
+ * @param sql
+ */
+void SqlBuilder::setSql(QString sql) { this->sql = sql; }
+/**
+ * @brief SqlBuilder::getSql
+ * @return
+ */
+QString SqlBuilder::getSql() { return this->sql; }
+
+/**
+ * @brief db
+ * @param table
+ * @return
+ */
 SqlBuilder* db(QString table) {
   return new SqlBuilder(table);
 }
