c68b87a5045c36f1f6cf7a56163d7d723b20907e
Moises Mariano <moisesduartem@gmail.com>
2020-12-10 14:11:09 -0300

    declare select() & sql getters and setters

--- SqlBuilder.h
+++ SqlBuilder.h
@@ -2,13 +2,20 @@
 #define SQLBUILDER_H
 
 #include <QString>
+#include <QStringList>
 
 class SqlBuilder
 {
 protected:
+    QString sql;
     QString table;
+    QStringList columns = {"*"};
 public:
     SqlBuilder(QString table);
+    SqlBuilder* select(QStringList columns = {"*"});
+
+    void setSql(QString sql);
+    QString getSql();
 };
 
 SqlBuilder* db(QString table);
