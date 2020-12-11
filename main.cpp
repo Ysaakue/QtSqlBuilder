64b832eb634e95156695b676fee7e99a30d4df7f
Moises Mariano <moisesduartem@gmail.com>
2020-12-11 14:53:36 -0300

    test query builder select with conditions

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <QCoreApplication>
 #include<QDebug>
 
+#include <SqlBuilder.h>
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     // return a.exec();
+
+    QVariantMap params;
+    params["id"] = 1;
+
+    qDebug() <<
+
+    db("users")
+      ->select({"id", "nome"})
+      ->where({"id", "=", ":id"}, params)
+      ->rows()
+
+             << "\n";
+
     return 0;
 }
